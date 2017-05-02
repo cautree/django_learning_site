6ec52faaeda7ec72a14e960a91fa80ea386ec15e
cautree <yanyanliujs@gmail.com>
2017-05-02 09:59:32 -0400

    add test

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Course
+
+class CourseModelTests(TestCase):
+	def test_course_creation(self):
+		course= Course.objects.create(
+			title="Python Regular Expression",
+			description ="Learn to write regular expression in Python")
+		now = timezone. now()
+		self.assertLess(course.created_at, now)
--- learning_site/urls.py
+++ learning_site/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     
-    url(r'^courses/', include('courses.urls')),
+    url(r'^courses/', include('courses.urls', namespace='courses')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.hello_world),
