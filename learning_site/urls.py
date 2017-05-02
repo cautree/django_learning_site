9bbfd9d86613c2a3a8a0a6f0b8dff4905135dd3b
cautree <yanyanliujs@gmail.com>
2017-05-01 23:57:54 -0400

    add css

--- learning_site/urls.py
+++ learning_site/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from . import views
 
 urlpatterns = [
@@ -9,3 +11,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.hello_world),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
