561f1100d3862146002ed5efd927cf904d36ae93
cautree <yanyanliujs@gmail.com>
2017-05-04 20:06:30 -0400

    fomrs

--- learning_site/forms.py
+++ learning_site/forms.py
@@ -1,17 +1,28 @@
 from django import forms
 from django.core import validators
 
+def must_be_empty(value):
+	if value:
+		raise forms.ValidationError('is not empty')
+
 class SuggestionForm(forms.Form):
 	name = forms.CharField()
 	email = forms.EmailField()
+	verify_email = forms.EmailField(label="Please verify your email address")
 	suggestion = forms.CharField(widget=forms.Textarea)
 	honeypot = forms.CharField(required=False,
 		                      widget=forms.HiddenInput,
-		                      label="Leave empty")
+		                      label="Leave empty",
+		                      validators =[must_be_empty])
 
-	def clean_honeypot(self):
-		honeypot = self.cleaned_data['honeypot']
-		if len(honeypot):
-			raise forms.ValidationError(
-				"honeypot should be left empty. Bad bot!")
-		return honeypot
+	# def clean(self):
+	# 	cleaned_data =super().clean()
+	# 	email = cleaned_data.get('email')
+	# 	verify = cleaned_data.get('verify_email')
+	# 	if email!=verify:
+	# 		raise forms.ValidationError("You need to enter the same email in both friends")
+  
+
+	# 
+
+	
